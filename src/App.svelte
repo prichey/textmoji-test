76f14233dacb250aab57948b8327a4fa0e24b4b0
Preston Richey <prestonleerichey@gmail.com>
2020-02-23 10:43:50 -0500

    Add color picker

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,15 @@
 <script>
-  import * as PIXI from "pixi.js";
-  import { onMount } from "svelte";
-  import { saveAs } from "file-saver";
+  import * as PIXI from 'pixi.js';
+  import { onMount } from 'svelte';
+  import { saveAs } from 'file-saver';
 
-  let container;
-  let text = "text moji";
+  let canvasRoot;
+  let text = 'text moji';
+  let bgColor = '#de3249';
   let dataUrl;
 
   onMount(() => {
-    container.appendChild(app.view);
+    canvasRoot.appendChild(app.view);
   });
 
   const config = { width: 100, height: 100 };
@@ -21,42 +22,58 @@
     preserveDrawingBuffer: true
   });
 
+  const container = new PIXI.Container();
+  container.sortableChildren = true;
+  app.stage.addChild(container);
+
   const bg = new PIXI.Graphics();
+  bg.zIndex = 0;
   const bgRadius = 10;
-  bg.beginFill(0xde3249);
+  bg.beginFill(PIXI.utils.string2hex(bgColor));
   bg.drawRoundedRect(0, 0, config.width, config.height, bgRadius);
   bg.endFill();
-  app.stage.addChild(bg);
+  container.addChild(bg);
+
   const padding = 5;
   const style = new PIXI.TextStyle({
-    fontFamily: "Arial",
+    fontFamily: 'Arial',
     fontSize: 40,
-    fill: "#fff",
+    fill: '#fff',
     wordWrap: true,
     wordWrapWidth: config.width - padding * 2,
-    fontWeight: "bold"
+    fontWeight: 'bold'
   });
 
   const basicText = new PIXI.Text(text, style);
+  basicText.zIndex = 10;
   basicText.x = padding;
   basicText.y = padding;
-  app.stage.addChild(basicText);
+  container.addChild(basicText);
   basicText.text = text;
 
   app.render();
-  dataUrl = app.view.toDataURL("image/png");
+  dataUrl = app.view.toDataURL('image/png');
 
   const updateText = () => {
     basicText.text = text;
     app.render();
-    dataUrl = app.view.toDataURL("image/png");
+    dataUrl = app.view.toDataURL('image/png');
+  };
+
+  const updateBgColor = () => {
+    bg.clear();
+    bg.beginFill(PIXI.utils.string2hex(bgColor));
+    bg.drawRoundedRect(0, 0, config.width, config.height, bgRadius);
+    bg.endFill();
+    container.addChild(bg);
+    dataUrl = app.view.toDataURL('image/png');
   };
 
   const download = () => {
     app.view.toBlob(blob => saveAs(blob, `textmoji-${getFilename(text)}.png`));
   };
 
-  const getFilename = str => str.replace(/([^a-z0-9]+)/gi, "");
+  const getFilename = str => str.replace(/([^a-z0-9]+)/gi, '');
 </script>
 
 <style>
@@ -68,10 +85,10 @@
 
 <main>
   <h1>textmoji</h1>
+  <input bind:value={text} on:input={updateText} type="text" />
   <div>
-    <input bind:value={text} on:input={updateText} type="text" />
-    <div bind:this={container} />
-    <!-- <canvas bind:this={view} /> -->
+    <input bind:value={bgColor} on:input={updateBgColor} type="color" />
+    <div bind:this={canvasRoot} />
   </div>
   <div>
     <img width="32" height="32" src={dataUrl} alt="" />
